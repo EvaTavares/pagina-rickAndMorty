b6e14399aa30c5d52d89348851169b8d6151a91a
Eva Tavares <eva.programadora@gmail.com>
2024-02-14 17:39:27 -0300

    add: método changePage

--- src/app/shared/services/api-service.service.ts
+++ src/app/shared/services/api-service.service.ts
@@ -13,4 +13,14 @@ export class ApiServiceService {
   getAllCharacters():Observable<any>{
     return this.http.get<any>(`${this.baseUrl}character`)
   }
+
+  changePage(page:number): Observable<any>{
+    const newPage = this.http.get<any>(`${this.baseUrl}character/?page=${page}`);
+    console.log(newPage)
+    return newPage
+  }
+
+  getCharacterLocation(id: number): Observable<any>{
+    return this.http.get(`${this.baseUrl}/location/${id}`)
+  }
 }
