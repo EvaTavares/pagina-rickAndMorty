1a4482a34854d1caaba9293fea6e721d32460801
Eva Tavares <eva.programadora@gmail.com>
2024-02-14 10:10:04 -0300

    feat: ApiService

---
+++ src/app/shared/services/api-service.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+  private baseUrl = 'https://rickandmortyapi.com/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCharacters():Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}character`)
+  }
+}
