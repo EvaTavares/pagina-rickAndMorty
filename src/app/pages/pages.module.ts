e427fae97ad4b162f224da832897cbc1e92a08ab
Eva Tavares <eva.programadora@gmail.com>
2024-02-14 12:58:18 -0300

    add: ComponentsModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
-
+import { ComponentsModule } from '../components/components.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
-    MatCardModule
+    MatCardModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
